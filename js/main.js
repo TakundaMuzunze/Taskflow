8feeee6218aed76c8cf5eb893f92d374a5ffb279
takmuzunze <takmuzunze@gmail.com>
2023-08-21 17:33:46 +0100

    update main.js - added function to display random tips on the dashboard for the user

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+// Displaying random tips on user dashboard
+const tipText = document.getElementById('tip-text');
+
+const tips = [
+    "Set clear goals to stay focused.",
+    "Take short breaks to recharge your mind.",
+    "Use a to-do list to keep track of tasks.",
+    "Strive for progress, not perfection, to avoid getting stuck on minor details.",
+    "Don't overload your list; keep it manageable to prevent overwhelm and burnout."
+];
+
+const randomTip = () => {
+    const randomIndex = Math.floor(Math.random() * tips.length);
+    tipText.textContent = "Tip of the Day: " + tips[randomIndex];
+}
+
+randomTip();
+
+// Function to handle user adding task
 const task = document.getElementById('single-task');
 const taskDisplayContainer = document.querySelector('.listed-tasks')
 const dateDisplay = document.querySelector('.date-display');
@@ -14,8 +33,11 @@ const addTask = (event) => {
         taskText.classList.add('task-content');
         taskText.innerText= `${taskInput}`;
 
-        const deleteIcon = document.createElement('i');
+        const deleteIcon = document.createElement('i'); // Added delete icon to task container so user is able to delete tasks
         deleteIcon.classList.add('fas', 'fa-trash', 'delete-icon');
+        deleteIcon.addEventListener("click", () => {
+            removeTasks(taskElementContainer);
+        });
 
         taskElementContainer.appendChild(taskText);
         taskElementContainer.appendChild(deleteIcon);
@@ -30,6 +52,7 @@ const addTask = (event) => {
     task.value="";
 };
 
+// Display task onto the dashboard page
 const displayTasks = () => {
     const currentDate = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
@@ -38,8 +61,9 @@ const displayTasks = () => {
     dateDisplay.textContent = formattedDate;
 }
 
-const removeTasks = () => {
-
+// Remove tasks
+const removeTasks = (taskElement) => {
+    taskDisplayContainer.removeChild(taskElement);
 }
 
 addIcon.addEventListener("click", addTask);
