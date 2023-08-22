2c8c7523c0cd1d3eadbbeeb23feac4b9ba9d51ee
takmuzunze <takmuzunze@gmail.com>
2023-08-22 11:44:26 +0100

    update registration.js - tweaked function to display specific error messages for each input field

--- js/registration.js
+++ js/registration.js
@@ -1,8 +1,12 @@
 const nameInput = document.getElementById('name-field');
 const emailInput = document.getElementById('email-field');
 const passwordInput = document.getElementById('pwd-field');
+const inputFields = document.querySelectorAll('.text-field');
 const submitButton  = document.getElementById('signup');
 const errorMessage = document.getElementById('error');
+const pwdErrorMessage = document.getElementById('error-pwd');
+const nameErrorMessage = document.getElementById('error-name');
+const emailErrorMessage = document.getElementById('error-email');
 
 const userDetails = [];
 
@@ -10,17 +14,27 @@ const handleRegistration = (event) => {
     event.preventDefault();
 
     if (emailInput.value === "" && passwordInput.value === "" && nameInput.value === "") {
-        errorMessage.textContent = "Please fill out the form with your details";
+        emailInput.classList.add('text-field-error');
+        nameInput.classList.add('text-field-error');
+        passwordInput.classList.add('text-field-error');
+        alert("Please fill out the form with your details to create an account");
         return false;
     }
 
+    else if(nameInput === ""){
+        nameErrorMessage.textContent = "Please enter your full name";
+        nameInput.classList.add('text-field-error');
+    }
+
     else if (emailInput.value === "" || !emailInput.value.includes("@")){
-        errorMessage.textContent = "Please enter a valid email address";
+        emailErrorMessage.textContent = "Please enter a valid email address"
+        emailInput.classList.add('text-field-error');
         return false;
     }
 
     else if (passwordInput.value.length < 7 || passwordInput === ""){
-        errorMessage.textContent = "Password must be at least than 7 characters";
+        pwdErrorMessage.textContent = "Password must be at least than 7 characters";
+        passwordInput.classList.add('text-field-error');
         return false;
     }
 
